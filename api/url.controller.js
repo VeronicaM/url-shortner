8c0f9104a586b49537c3c905fabc74dbc20aef1f
Mihai <mihaiveronica@ymail.com>
2017-11-07 13:09:00 +0200

    switch url shortening to use entries count instead of entry id

--- api/url.controller.js
+++ api/url.controller.js
@@ -30,22 +30,30 @@ exports.handleURL = function(req, res) {
 }
 
 function getURL(req, res) {
-    url.findOne({ $or: [{ original_url: req.params[0] }, { short_url: req.params[0] }] }, function(error, data) {
-        if (error) {
-            return res.status(500).send(error);
-        }
-        if (data) {
-            var newTag = new Object();
-            newTag.original_url = data.original_url;
-            newTag.short_url = data.short_url;
-            return res.status(200).send(newTag);
-        } else {
-            var newTag = new Object();
-            newTag.original_url = req.params[0];
-            newTag.short_url = "https://" + req.hostname + "/" + mongoose.Types.ObjectId();
-            url.create(newTag, function(err, createdTag) {
+    url.findOne({ $or: [{ original_url: req.params[0] }, { short_url: req.params[0] }] },
+        function(error, data) {
+            if (error) {
+                return res.status(500).send(error);
+            }
+            if (data) {
+                var newTag = new Object();
+                newTag.original_url = data.original_url;
+                newTag.short_url = data.short_url;
                 return res.status(200).send(newTag);
-            });
-        }
+            } else {
+                var newTag = new Object();
+                newTag.original_url = req.params[0];
+                url.count({}, function(error, count) {
+                    if (error) return res.send({ error: "Something went wrong" });
+                    return saveURL(req, res, count, newTag)
+                });
+            }
+        });
+}
+
+function saveURL(req, res, count, newTag) {
+    newTag.short_url = "https://" + req.hostname + "/" + count;
+    url.create(newTag, function(err, createdTag) {
+        return res.status(200).send(newTag);
     });
 }
